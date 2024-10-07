ec16bf165303a3a50f89fc6c13572875df2b73ce
iam-deepaksharma <deepak30192@gmail.com>
2024-10-08 02:06:18 +0530

    update

---
+++ shell script/07_string_ops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+myVar="Hey Buddy, How are you?"
+
+myVarLength=${#myVar}
+echo "Length of the myVar is $myVarLength"
+
+echo "Upper case is ${myVar^^}"
+echo "Lower case is ${myVar,,}"
+
+#To replace a string
+newVar=${myVar/Buddy/Deepak}
+echo "New Var is $newVar"
